8a969f265855067c3a071ca98a1869964842b630
CleveHardcastle <treyhardcastle@gmail.com>
2022-11-23 19:19:22 -0800

    finished project

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,21 @@ var geoUrl =
 var cityUrl =
   "http://api.openweathermap.org/data/2.5/forecast?&units=imperial&appid=99886c59c78ad6d91177b0fde1bce458";
 
+const d = new Date();
+let day = d.getDate();
+let month = d.getMonth() + 1;
+var dayMonth = month + "/" + day;
+var card1 = document.getElementById("card1");
+var card2 = document.getElementById("card2");
+var card3 = document.getElementById("card3");
+var card4 = document.getElementById("card4");
+
+card1.textContent = month + "/" + (day + 1);
+card2.textContent = month + "/" + (day + 2);
+card3.textContent = month + "/" + (day + 3);
+card4.textContent = month + "/" + (day + 4);
+
+
 function getResults() {
   userInput = query.value;
 
@@ -22,17 +37,14 @@ function getResults() {
       return response.json();
     })
     .then(function (data) {
-      console.log(data.list[3].main.temp);
-      console.log(data.list[11].main.temp);
-      console.log(data.list[19].main.temp);
-      console.log(data.list[27].main.temp);
-      console.log(data.list[35].main.temp);
+      document.getElementById("today").textContent = userInput + "'s Weather today " + dayMonth + ": " + data.list[3].main.temp + "°F " + " Humidity: " + data.list[3].main.humidity + "% " + " Wind Speeds: " + data.list[3].wind.speed + "MPH";
+      document.getElementById("text1").textContent = "Temp: " + data.list[11].main.temp + "°F " + " Humidity: " + data.list[11].main.humidity + "% " + " Wind Speeds: " + data.list[11].wind.speed + "MPH";
+      document.getElementById("text2").textContent = "Temp: " + data.list[19].main.temp + "°F " + " Humidity: " + data.list[19].main.humidity + "% " + " Wind Speeds: " + data.list[19].wind.speed + "MPH";
+      document.getElementById("text3").textContent = "Temp: " + data.list[27].main.temp + "°F " + " Humidity: " + data.list[27].main.humidity + "% " + " Wind Speeds: " + data.list[27].wind.speed + "MPH";
+      document.getElementById("text4").textContent = "Temp: " + data.list[35].main.temp + "°F " + " Humidity: " + data.list[35].main.humidity + "% " + " Wind Speeds: " + data.list[35].wind.speed + "MPH";
     });
-
-  var townName = userInput;
-  var entry = document.createElement("li");
-  entry.appendChild(document.createTextNode(townName));
-  towns.appendChild(entry);
 }
+
+
 
 btn.addEventListener("click", getResults);
